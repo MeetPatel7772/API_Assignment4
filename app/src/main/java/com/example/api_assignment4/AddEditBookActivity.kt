b6bc72207aa159a81d4f23f1a30e162327570198
AMD Akshil <amdakshil@Mac.local>
2023-12-01 12:59:08 -0500

    UI for login and register user

---
+++ app/src/main/java/com/example/api_assignment4/AddEditBookActivity.kt
@@ -0,0 +1,11 @@
+package com.example.api_assignment4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddEditBookActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit_book)
+    }
+}
